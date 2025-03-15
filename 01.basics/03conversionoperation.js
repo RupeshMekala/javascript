2ec48ad57a2c061ff7acd1b47b36bb40c591a9ef
RupeshMekala <157112595+RupeshMekala@users.noreply.github.com>
2025-03-15 04:31:06 +0000

    1

---
+++ 01.basics/03conversionoperation.js
@@ -0,0 +1,16 @@
+let score = "33abc"
+
+// console.log(typeof score); // score = 33
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score) // score = "33"
+// console.table([valueInNumber, typeof valueInNumber]);
+
+// let valueInNumber = Number(score) // score = "33abc"
+// console.log(typeof valueInNumber, valueInNumber);
+
+/**
+ Nan means not a number but it's type is a number
+ */
+
+ 
