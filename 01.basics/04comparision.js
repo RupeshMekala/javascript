6fc6a180e365932d420e944efded553c2529953a
RupeshMekala <157112595+RupeshMekala@users.noreply.github.com>
2025-03-15 10:06:03 +0000

    1

---
+++ 01.basics/04comparision.js
@@ -0,0 +1,15 @@
+// comparision operators >, <, >=, <=, ==, !=
+// === --> true if operands are equal and of same type
+// !== --> true if operands are not equal but are of same type
+
+// console.log("2" > 1) // it may raise an error, so better to compare same datatypes
+
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0) // what are the results and why?
+
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined >= undefined)
