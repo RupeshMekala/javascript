d2eb4094d52ec98350659de054520c9d766b304b
RupeshMekala <157112595+RupeshMekala@users.noreply.github.com>
2025-03-15 04:54:52 +0000

    1

--- 01.basics/03conversionoperation.js
+++ 01.basics/03conversionoperation.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+let score = "rupesh"
 
 // console.log(typeof score); // score = 33
 // console.log(typeof(score));
@@ -13,4 +13,39 @@ let score = "33abc"
  Nan means not a number but it's type is a number
  */
 
- 
+// let valueInNumber = Number(score) // score = null
+// console.log(typeof valueInNumber, valueInNumber);
+
+// let valueInNumber = Number(score) // score = undefined
+// console.log(typeof valueInNumber, valueInNumber);
+
+// let valueInNumber = Number(score) // score = true
+// console.log(typeof valueInNumber, valueInNumber);
+
+// let valueInNumber = Number(score) // score = "string"
+// console.log(typeof valueInNumber, valueInNumber);
+
+
+/*
+ notes
+ "33" --> 33
+ "33abc" --> NaN
+ true --> 1, false --> 0
+ */
+
+let isLoggenIn = ""
+
+// let booleanisLoggedIn = Boolean(isLoggenIn) // isLoggedIn = 1
+// console.log(typeof booleanisLoggedIn, booleanisLoggedIn);
+
+let booleanisLoggedIn = Boolean(isLoggenIn) // isLoggedIn = ""
+console.log(typeof booleanisLoggedIn, booleanisLoggedIn);
+
+
+// boolean
+// 1 --> true, 0 --> false
+// "" --> false, " " --> true, "string" --> true
+
+
+// every basic datatype can be converted to string
+
