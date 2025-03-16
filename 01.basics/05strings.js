31da7ade277b25c9643b1b12c1c18ced04baad32
RupeshMekala <157112595+RupeshMekala@users.noreply.github.com>
2025-03-16 06:55:07 +0000

    1

---
+++ 01.basics/05strings.js
@@ -0,0 +1,9 @@
+const name = "Rupesh"
+
+const repoCount = 30
+
+// console.log(name + " Value:" + repoCount) // syntax not recommended
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+// string interpolation, placeholders are created for variables inside string (``)
+
