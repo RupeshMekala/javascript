4940f676c2907d70d8a58737d1bd22215c3fca5c
RupeshMekala <157112595+RupeshMekala@users.noreply.github.com>
2025-03-16 04:26:46 +0000

    1

--- 01.basics/datatypes.summary.js
+++ 01.basics/datatypes.summary.js
@@ -22,20 +22,45 @@ console.log(typeof bigNumber);
 
 // arrays, objects, functions
 
-const heros = ["thor", "hulk", "ironman"]
+// const heros = ["thor", "hulk", "ironman"]
 
-let myObject = {
-    name : "Rupesh",
-    age : 20,
-    mail : "rupesh.dev@gmail.com"
-}
+// let myObject = {
+//     name : "Rupesh",
+//     age : 20,
+//     mail : "rupesh.dev@gmail.com"
+// }
 
-const myFunction = function(){
-    console.log("Hello world!")
-}
+// const myFunction = function(){
+//     console.log("Hello world!")
+// }
 
 
-console.log(typeof(myFunction))
+// console.log(typeof(myFunction))
 
 // what is the return type of all the primitive and reference datatypes?
+
+
+// ******************************************************************************************
+
+// Stack(used for primitive datatypes) //Heap(used for reference datatypes)
+// when stack memory is used, a copy of the original one is sent rather than original itself
+
+let nameOne = "Rupesh"
+let nameTwo = nameOne
+nameTwo = "Virat Kohli"
+console.table([nameOne, nameTwo]) //what hapens?
+
+// when heap memory is used, the reference value is sent(means the original value)
+
+let userOne = {
+    name : "Rupesh",
+    age : 20,
+    mail : "rupesh@anotherId.com",
+}
+
+let userTwo = userOne
+
+userTwo.mail = "rupesh@dev.com"
+
+console.table([userOne.mail, userTwo.mail]) // what happens and why ?
 
