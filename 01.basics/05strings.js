b0b2829eda485cb174eb21b169ca50d3ad3da9c5
RupeshMekala <157112595+RupeshMekala@users.noreply.github.com>
2025-03-18 06:06:54 +0000

    1

--- 01.basics/05strings.js
+++ 01.basics/05strings.js
@@ -7,3 +7,29 @@ const repoCount = 30
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
 // string interpolation, placeholders are created for variables inside string (``)
 
+const gameName = new String('rupesh')
+
+console.log(gameName[3]);
+console.log(gameName.__proto__)
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+
+console.log(gameName.indexOf('e'))
+console.log(gameName.charAt(5))
+
+console.log(gameName.substring(0, 4)) //can -ve index be passed?
+console.log(gameName.slice(-5, -1))
+
+
+const newName = "    Rupesh    "
+console.log(newName)
+console.log(newName.trim())
+
+const url = "https://www.google.com/hitesh%20ch-oud-ary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('hitesh'))
+
+console.log(url.split('-'))
